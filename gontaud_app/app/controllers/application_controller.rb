93cad524676bd3269324ec731a8aa4d2f940384d
antontre1 <antonin@folliasson.com>
2021-09-01 15:04:15 +0200

    merge avec withAR61

--- gontaud_app/app/controllers/application_controller.rb
+++ gontaud_app/app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
-    binding.pry
+    # binding.pry
   end
 
 
